4bbcbe019e3a8da9d6c9a7ed34f8b722c0b0c8e1
Alex Johnson <johnsonav1992@gmail.com>
2024-04-27 13:47:11 -0500

    ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent {
     }
   ];
 
-  filter = 'all';
+  filter = '';
 
   get visibleItems() {
     let value = this.filter;
--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FilterOptions } from '../../../shared/types/types';
 import { WishItem } from '../../../shared/models/wishItem';
@@ -10,11 +10,15 @@ import { WishItem } from '../../../shared/models/wishItem';
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.css'
 })
-export class FilterComponent {
+export class FilterComponent implements OnInit {
   @Output() filterWishes = new EventEmitter<FilterOptions>();
   @Input() items: WishItem[] = [];
 
   listFilter: FilterOptions = 'all';
+
+  ngOnInit(): void {
+    this.filterWishes.emit('all')
+  }
 
   changeFilter(value: FilterOptions) {
     this.filterWishes.emit(value)
